b120ba53540396e7dc7af616cf3d38b4d38c893c
QuentinDll <quentin.delplace13@gmail.com>
2018-07-31 14:32:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 3</title>
+</head>
+<body>
+  <p>
+    <?php
+    $numbOne = 100;
+    $numbTwo = 50;
+
+    while ($numbOne >= 10){
+      echo $numbOne * $numbTwo ." ";
+      echo $numbOne -- ." ";
+    }
+    ?>
+  </p>
+</body>
+</html>
